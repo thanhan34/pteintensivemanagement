62c5e841550ed4f42af89d63231df4c0ea82606e
thanhan34 <dtan42@gmail.com>
2024-12-05 12:51:07 +0700

    fix error to push

--- app/components/StudentList.tsx
+++ app/components/StudentList.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useMemo } from 'react';
 import { Student } from '../types/student';
 import { useSession } from 'next-auth/react';
 
